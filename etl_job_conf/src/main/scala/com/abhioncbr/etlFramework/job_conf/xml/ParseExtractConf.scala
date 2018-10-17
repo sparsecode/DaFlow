782a72eef3e7cc6b374fa3a1479c3a3f4f3cee02
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2018-10-16 20:28:10 -0400

    Further refactored code of job xml file evaluation.

--- etl_job_conf/src/main/scala/com/abhioncbr/etlFramework/job_conf/xml/ParseExtractConf.scala
+++ etl_job_conf/src/main/scala/com/abhioncbr/etlFramework/job_conf/xml/ParseExtractConf.scala
@@ -1,35 +1,26 @@
 package com.abhioncbr.etlFramework.job_conf.xml
 
-import com.abhioncbr.etlFramework.commons.common.file.{FileNameParam, FilePath, PathInfixParam}
+import com.abhioncbr.etlFramework.commons.common.file.FilePath
 import com.abhioncbr.etlFramework.commons.common.query.{Query, QueryFileParam, QueryParam}
-import com.abhioncbr.etlFramework.commons.util.FileUtil
 import com.abhioncbr.etlFramework.commons.extract.{Extract, ExtractionType}
 
 object ParseExtractConf {
   def fromXML(node: scala.xml.NodeSeq): Extract = {
     val extractionType = ExtractionType.getValueType((node \ "type").text)
-
-    var extract: Extract = null
-    if(extractionType.equals(ExtractionType.JSON)) {
-      extract = Extract(extractionType,
-        dataPath = Some(
+    val extract: Extract = Extract(extractionType = extractionType,
+        dataPath =  Some(
           FilePath(pathPrefix = Some((node \ "file_initial_path").text),
             groupPatterns = None,
             feedPattern = None, //Some(PathInfixParam((node \ "file_name_pattern").text, Some((node \ "format_file_name").text.toBoolean))),
             fileName = None //Some(FileNameParam(Some((node \ "file_prefix").text)))
           )
         ),
-        query = None,
-        validateExtractedData = (node \ "validate_extracted_data").text.toBoolean)
-    } else if(extractionType.equals(ExtractionType.JDBC)) {
-      extract = Extract(extractionType = extractionType,
-        dataPath = None,
         query = Some(Query(
-          QueryFileParam(Some(FileUtil.getFilePathObject((node \ "db_property_file_path").text)), Some(FileUtil.getFilePathObject((node \ "sql_query_file_path").text))),
-          Some(Array[QueryParam]((node \ "query_params" \ "param").toList map { s => ParseQueryParam.fromXML(s) }: _*))
+          queryFile = QueryFileParam(configurationFile= ParseUtil.parseFilePathString((node \ "db_property_file_path").text),
+            queryFile= ParseUtil.parseFilePathString((node \ "sql_query_file_path").text)),
+          queryArgs = Some(Array[QueryParam]((node \ "query_params" \ "param").toList map { s => ParseQueryParam.fromXML(s) }: _*))
         )),
-        validateExtractedData = (node \ "validate_extracted_data").text.toBoolean)
-    }
+        validateExtractedData = ParseUtil.parseBoolean((node \ "validate_extracted_data").text))
     extract
   }
 }
--- etl_job_conf/src/main/scala/com/abhioncbr/etlFramework/job_conf/xml/ParseRule.scala
+++ etl_job_conf/src/main/scala/com/abhioncbr/etlFramework/job_conf/xml/ParseRule.scala
@@ -21,9 +21,9 @@ object ParseRule {
   }
 
   def fromXML(node: scala.xml.NodeSeq): DummyRule = {
-    val order = (node \ "@order").text.toInt
+    val order = ParseUtil.parseInt((node \ "@order").text)
     val ruleType  = (node \ "@type").text
-    val group = (node \ "@group").text.toInt
+    val group = ParseUtil.parseInt((node \ "@group").text)
     val condition = (node \ "condition").text
 
     val rule = ruleType match {
@@ -48,8 +48,8 @@ object ParseRule {
         val subRules: List[DummyRule] = List[DummyRule]((node \ "rule").toList map { s => ParseRule.fromXML(s) }: _*)
         DummyRule(new PartitionRule(order, scope, condition, group), subRules)
 
-      case "SCHEMA_TRANSFORMATION" => val failedFieldLimit = (node \ "@failed_field_limit").text.toInt
-        val failedRowLimit = (node \ "@failed_row_limit").text.toInt
+      case "SCHEMA_TRANSFORMATION" => val failedFieldLimit = ParseUtil.parseInt((node \ "@failed_field_limit").text)
+        val failedRowLimit = ParseUtil.parseInt((node \ "@failed_row_limit").text)
         val mappings = List[FieldMapping]((node \ "field_mapping").toList map { s => ParseFieldMapping.fromXML(s) }: _*)
         DummyRule(new SchemaTransformationRule(order, condition, group, mappings, failedFieldLimit, failedRowLimit), null)
 
