e1edfb991e3c92394178d7f412064e2202d83c41
Abhishek Sharma <abhisheksharma@Abhisheks-MacBook-Pro.local>
2018-10-30 19:26:56 -0400

    Added unit test-cases for Load job conf.

--- etl_job_conf/src/main/scala/com/abhioncbr/etlFramework/jobConf/xml/ParseLoad.scala
+++ etl_job_conf/src/main/scala/com/abhioncbr/etlFramework/jobConf/xml/ParseLoad.scala
@@ -11,11 +11,11 @@ object ParseLoad {
 
 object ParseLoadFeed {
   def fromXML(node: scala.xml.NodeSeq): LoadFeed = {
-    val loadFeedName : String = (node \ "@feedName").text
+    val loadFeedName : String = (node \ "@name").text
     val loadType: LoadType.valueType = LoadType.getValueType( valueTypeString = (node \ "_").head.label.toUpperCase)
     val attributesMap: Map[String, String] = (node \ "_").head.attributes.map(meta => (meta.key, meta.value.toString)).toMap
     val dataPath: FilePath = ParseUtil.parseNode[FilePath](node \ "_" \ "dataPath", None, ParseDataPath.fromXML).orNull
-    val partitioningData: Option[PartitioningData] = ParseUtil.parseNode[PartitioningData](node \ "hive" \"partition_data", None, ParsePartitioningData.fromXML)
+    val partitioningData: Option[PartitioningData] = ParseUtil.parseNode[PartitioningData](node \ "hive" \"partitionData", None, ParsePartitioningData.fromXML)
 
     val feed: LoadFeed = LoadFeed(loadFeedName = loadFeedName, loadType = loadType, attributesMap = attributesMap, dataPath = dataPath, partitioningData = partitioningData)
     feed
